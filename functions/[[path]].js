ba742c69a71b8a7d06f26e85d3b41e918f7daa08
mvtruyen2003 <120457285+mvtruyen2003@users.noreply.github.com>
2026-07-23 23:41:44 +0700

    Update [[path]].js

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -1,6 +1,6 @@
 // ==================== CONFIG ====================
-const UPSTREAM_PRIMARY = 'https://pxj0pm97xo.cloudflare-gateway.com/dns-query';
-const UPSTREAM_FALLBACK = 'https://euw4882anx.cloudflare-gateway.com/dns-query';
+const UPSTREAM_PRIMARY = 'https://euw4882anx.cloudflare-gateway.com/dns-query';
+const UPSTREAM_FALLBACK = 'https://pxj0pm97xo.cloudflare-gateway.com/dns-query';
 const UPSTREAM_GEO_BYPASS = 'https://dns.mullvad.net/dns-query'; // Re-resolve without ECS when geo-block returns loopback
 const UPSTREAM_TIMEOUT = 5000;
 
