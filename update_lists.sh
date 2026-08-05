4cbd8269cb98669c9974d0860b19a322f353ec1f
mvtruyen2003 <120457285+mvtruyen2003@users.noreply.github.com>
2026-08-06 00:09:48 +0700

    Update update_lists.sh

--- update_lists.sh
+++ update_lists.sh
@@ -32,6 +32,7 @@ extract_domains() {
 echo "Downloading and processing blocklists..."
 curl -fsSL --max-time 60 \
 https://raw.githubusercontent.com/bibicadotnet/blocklist_minimal/main/blocklists.txt \
+https://raw.githubusercontent.com/mvtruyen2003/Dns/refs/heads/main/Block \
 | extract_domains > "$BLOCK_TMP"
 
 echo "Downloading and processing allowlists..."
